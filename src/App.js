3ed3d741c57a7cd4eb26f7b4274b942f71099cfc
sandro <seivanesandro@gmail.com>
2022-09-26 19:48:27 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/index.css 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const BackTitle = styled.div`
+border: 0;
+box-shadow: 1px 1px 1px 1px black;
+margin-left:25em;
+width:50%;
+display:flex;
+justify-content: space-between;
+`;
+
+
+const Title = styled.h3`
+color:red;
+padding: 1em;
+padding-left: 3em;
+text-align: left;
+`;
+
+const Title3 = styled.h3`
+color:red;
+padding: 1em;
+padding-right: 3em;
+text-align: right;
+
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <BackTitle>
+       <Title>styled components</Title>
+       <Title3>styled components</Title3>
+     </BackTitle>
+
     </div>
   );
 }
